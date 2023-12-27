2c0fbe285bbf74299dffe206c7ff7f4613a73570
Fahad Mansuri <fahadmansuri.work@gmail.com>
2023-12-27 16:25:34 +0100

    Project completed

---
+++ 14 - ByteCoin/ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Fahad Mansuri on 19.06.23.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Codable {
+    let rate: Double
+}
---
+++ 14 - ByteCoin/ByteCoin/Model/CoinModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoinModel.swift
+//  ByteCoin
+//
+//  Created by Fahad Mansuri on 29.06.23.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinModel {
+    let rate: Double
+}
