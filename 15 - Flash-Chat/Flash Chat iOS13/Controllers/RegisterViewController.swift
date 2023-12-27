1ab486e5d9b553b41315773fe18b332b6fdf1f67
Fahad Mansuri <fahadmansuri.work@gmail.com>
2023-12-27 16:47:57 +0100

    first commit

---
+++ 15 - Flash-Chat/Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -0,0 +1,41 @@
+//
+//  RegisterViewController.swift
+//  Flash Chat iOS13
+//
+//  Created by Angela Yu on 21/10/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class RegisterViewController: UIViewController {
+
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    
+    @IBAction func registerPressed(_ sender: UIButton) {
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    print(e)
+                    
+                    // Create a new alert
+                    var dialogMessage = UIAlertController(title: "Attention", message: e.localizedDescription, preferredStyle: .alert)
+                    // Create OK button with action handler
+                     let ok = UIAlertAction(title: "OK", style: .default, handler: { (action) -> Void in
+                         print("Ok button tapped")
+                      })
+                     //Add OK button to a dialog message
+                     dialogMessage.addAction(ok)
+                    // Present alert to user
+                    self.present(dialogMessage, animated: true, completion: nil)
+                    
+                } else {
+                    self.performSegue(withIdentifier: K.registerSegue, sender: self)
+                }
+            }
+        }
+    }
+    
+}
