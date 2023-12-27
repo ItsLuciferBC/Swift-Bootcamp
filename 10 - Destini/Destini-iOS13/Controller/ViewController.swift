0e8d0afba450910932c6a041808427e00c7cbf96
Fahad Mansuri <fahadmansuri.work@gmail.com>
2023-12-27 16:21:52 +0100

    Project completed

---
+++ 10 - Destini/Destini-iOS13/Controller/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Destini-iOS13
+//
+//  Created by Angela Yu on 08/08/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    
+    var storyBrain = StoryBrain()
+    /*let story0 = "You see a fork in the road."
+    let choice1 = "Take a left."
+    let choice2 = "Take a right."*/
+    
+    
+    
+    override func viewDidLoad() {
+        updateUI()
+    }
+
+    @IBAction func choiceMade(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle!
+        storyBrain.nextStory (userChoice: userAnswer)
+        updateUI()
+    }
+    
+    func updateUI(){
+        storyLabel.text = storyBrain.getStoryText()
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
+    }
+    
+}
+
